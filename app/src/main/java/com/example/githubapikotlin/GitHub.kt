aaa27b228d4c8dd1fc19f56f310e4df0d55c949d
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-01-22 07:37:28 +0900

    リファクタリング

---
+++ app/src/main/java/com/example/githubapikotlin/GitHub.kt
@@ -0,0 +1,5 @@
+package com.example.githubapikotlin
+
+data class GitHub(val login: String,
+                  val name: String,
+                  val location: String)
--- app/src/main/java/com/example/githubapikotlin/MainActivity.kt
+++ app/src/main/java/com/example/githubapikotlin/MainActivity.kt
@@ -30,39 +30,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun main() {
-        println("debug, start!")  // 1
         GlobalScope.launch {
             launch {
-                println("debug, coroutine start!") // 3
-                println("debug, ${fetch()}") // 4
                 val gitHub = fetch()
-                binding.nameTextView.text = gitHub.name
-                binding.loginTextView.text = gitHub.login
-                binding.locationTextView.text = gitHub.location
-                println("debug, coroutine end!") // 5
+                gitHub?.let {
+                    binding.nameTextView.text = gitHub.name
+                    binding.loginTextView.text = gitHub.login
+                    binding.locationTextView.text = gitHub.location
+                }
             }
         }
-        println("debug, stop!") // 2
     }
 
     // 中断するものにsuspendつける
-    suspend fun fetch(): GitHubModel {
+    suspend fun fetch(): GitHub? {
         val urlString = "https://api.github.com/users/tatsuya-ss"
         val (_, _, result) = urlString.httpGet().responseString()
         return when(result) {
             is Result.Failure -> {
                 println(result.getException().toString())
-                return GitHubModel("不明", "不明", "不明")
+                return null
             }
             is Result.Success -> {
                 val jsonResult = result.get()
-                var gitHub = Gson().fromJson(jsonResult, GitHubModel::class.java)
+                var gitHub = Gson().fromJson(jsonResult, GitHub::class.java)
                 return gitHub
             }
         }
     }
+
 }
 
-data class GitHubModel(val login: String,
-                       val name: String,
-                       val location: String)
